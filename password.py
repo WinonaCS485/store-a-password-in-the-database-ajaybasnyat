68eee185d8acc77e9be71e6582451759e5f4312c
Ajay Basnyat <ajaybasnyat@gmail.com>
2020-02-05 15:07:33 -0600

    comments

--- password.py
+++ password.py
@@ -1,24 +1,27 @@
 import uuid, hashlib, pymysql.cursors
 
+# get user input
 password = input("Enter password: ")
+
+#create salt
 salt = str(uuid.uuid4().hex) 
 
 # open-source method to ONE-WAY hash a password
 hashed_password = hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
-
+# convert to string
 hashString = str(hashed_password)
 
 # Connect to the database
 connection = pymysql.connect(host='mrbartucz.com',
                              user='fx9785rk',
-                             password='Pernambucano.wolf',
+                             password='',
                              db='fx9785rk_Password',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
 try:
     with connection.cursor() as cursor:
-        # Select all Students
+        # insert hash and salt into DB
         sql = "INSERT INTO `Passwords` VALUES (%s, %s)"
         
         # execute the SQL command
@@ -29,12 +32,16 @@ try:
 finally:
     connection.close()
 
+# get new uesr input
 password = input("Enter password: ")
 
+# update hash
 hashed_password = hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
 
+# convert to stirng
 newHashString = str(hashed_password)
 
+# output statement based on password entered
 if hashString == newHashString:
     print("Password is correct!")
 else:
